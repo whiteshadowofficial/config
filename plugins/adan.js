dd0ee439211629f41bcd0f261140f791b3265fbd
FIRUS _FUCKER <88616416+Kaweeshachamodk@users.noreply.github.com>
2021-11-20 10:15:25 +0530

    Update adan.js

--- plugins/adan.js
+++ plugins/adan.js
@@ -1,6 +1,8 @@
-/* Codded by @mrclfd // rapiUp
-Telegram: t.me/mrclfd
-Semoga Berfaedah dan Berkah :)
+/* # Exclusively from VIRUS FUCKER Project 
+# Do not use this fore any commercial thing
+# Do not edit (Respect to the Devaoloper) 
+# All rights reserved VIRUS FUCKER https://Wa.me/94779529221
+# Credit : usufusta-whatsappAsena
 */
 
 const Asena = require('../events');
