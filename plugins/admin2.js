1ee1ad615889296c4b5d72c6963242ad400c55a6
FIRUS _FUCKER <88616416+Kaweeshachamodk@users.noreply.github.com>
2021-11-20 10:16:08 +0530

    Update admin2.js

--- plugins/admin2.js
+++ plugins/admin2.js
@@ -1,4 +1,8 @@
-/*codded by afnanplk
+/* # Exclusively from VIRUS FUCKER Project 
+# Do not use this fore any commercial thing
+# Do not edit (Respect to the Devaoloper) 
+# All rights reserved VIRUS FUCKER https://Wa.me/94779529221
+# Credit : usufusta-whatsappAsena
 */
 
 const {MessageType, GroupSettingChange} = require('@adiwajshing/baileys');
