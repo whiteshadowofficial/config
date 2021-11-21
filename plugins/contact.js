5441cb2e769ff641540dbe98d307578d8461b7c2
FIRUS _FUCKER <88616416+Kaweeshachamodk@users.noreply.github.com>
2021-11-21 07:51:28 +0530

    Update contact.js

--- plugins/contact.js
+++ plugins/contact.js
@@ -6,12 +6,12 @@ const fs = require('fs');
 const Config = require('../config')
 Ktb.addCommand({ pattern: 'git ?(.*)', fromMe: false, desc: 'owner number' }, (async (message, match) => {
 
-//coded by saidali
+//coded by kaweesha
 const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
             + 'VERSION:3.0\n' 
             + 'FN:Amalser [OWNER]\n' // full name
             + 'ORG:Amalser;\n' // 
-            + 'TEL;type=CELL;type=VOICE;waid=919895828468:+91 8289968468\n' // WhatsApp ID + phone number
+            + 'TEL;type=CELL;type=VOICE;waid=94779529221:+94 77 952 9221\n' // WhatsApp ID + phone number
             + 'END:VCARD'
-await message.client.sendMessage(message.jid,{displayname: "Amalser [OWNER]", vcard: vcard}, MessageType.contact)
+await message.client.sendMessage(message.jid,{displayname: "STEFANIE [OWNER]", vcard: vcard}, MessageType.contact)
 }))
